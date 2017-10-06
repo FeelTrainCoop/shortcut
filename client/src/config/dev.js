4881a4ff2067aa502a8c680b17303ca726ca065d
Darius Kazemi <darius.kazemi@gmail.com>
2017-10-06 16:40:52 -0700

    Updating config

--- client/src/config/dev.js
+++ client/src/config/dev.js
@@ -5,13 +5,8 @@ import baseConfig from './base';
 let config = {
   appEnv: 'dev',
   authServer: 'http://localhost:3000',
-  // apiEndpoint: 'https://3rr1yij7g0.execute-api.us-east-1.amazonaws.com/staging/',
-  // apiEndpoint: 'https://3rr1yij7g0.execute-api.us-east-1.amazonaws.com/dev/',
-  // apiEndpoint: 'https://lu49rv1wa8.execute-api.us-west-2.amazonaws.com/shortcutdev/',
-  // apiEndpointBackup: 'https://wanz89yyu8.execute-api.us-east-1.amazonaws.com/shortcutdev/',
   apiEndpoint: 'http://localhost:3000',
-  //dataBucket: 'https://d1bojkr1jokyfh.cloudfront.net/'
-  dataBucket: 'http://localhost/explainjojo-assets/',
+  dataBucket: 'http://localhost:8080/',
   s3Region: 'us-west-2',
   s3Bucket: 'shortcut-getting-started',
 };
